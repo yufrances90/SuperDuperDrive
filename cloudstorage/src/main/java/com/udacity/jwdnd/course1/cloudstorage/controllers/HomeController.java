511abbf7962c067efd791fe243a70a4ff7bbcb43
Beijia(Frances) Yu <43866367+yufrances90@users.noreply.github.com>
2020-07-05 15:14:51 -0700

    feat: Resolves #1 (2)

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,8 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import com.udacity.jwdnd.course1.cloudstorage.models.UserVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,18 +15,23 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/home")
     public String getHomepage(Model model) {
         return "home";
     }
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String getLoginPage(Model model) {
         return "login";
     }
 
-    @RequestMapping("/signup")
-    public String getSignupPage(Model model) {
+    @GetMapping("/signup")
+    public String signupForm(
+            @ModelAttribute("userVo") UserVO userVo,
+            Model model
+    ) {
 
         Map<String, Object> data = new HashMap<>();
 
@@ -31,5 +41,16 @@ public class HomeController {
         model.addAllAttributes(data);
 
         return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupSubmit(
+            @ModelAttribute("userVo") UserVO userVo,
+            Model model
+    ) {
+
+        this.logger.error("Received user info from Signup Form: " + userVo.toString());
+
+        return "home";
     }
 }
---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/UserVO.java
@@ -0,0 +1,61 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class UserVO {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public UserVO() {
+    }
+
+    public UserVO(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserVO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
