812db6cccb037a7743cc9c9313774496829d3fe6
Beijia(Frances) Yu <43866367+yufrances90@users.noreply.github.com>
2020-07-06 19:43:39 -0700

    feat: Resolved #15

--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/workflows/LoginAndSignupWorkflowTests.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/workflows/LoginAndSignupWorkflowTests.java
@@ -11,8 +11,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import java.time.Duration;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginAndSignupWorkflowTests {
 
@@ -48,7 +46,7 @@ public class LoginAndSignupWorkflowTests {
     }
 
     @Test
-	public void login() throws InterruptedException {
+	public void loginWithInvalidCredentials() throws InterruptedException {
 
 		driver.get("http://localhost:" + this.port + "/login");
 
@@ -60,32 +58,103 @@ public class LoginAndSignupWorkflowTests {
 
         WebElement usrnameField = driver.findElement(By.id("inputUsername"));
 
-        Thread.sleep(1000);
-
         usrnameField.sendKeys("byu00");
-
-        Thread.sleep(1000);
 
         WebElement pwdField = driver.findElement(By.id("inputPassword"));
 
-        Thread.sleep(1000);
-
         pwdField.sendKeys("test123");
-
-        Thread.sleep(1000);
 
         WebElement submitButton = driver.findElement(By.id("loginBtn"));
 
         Assertions.assertEquals("Login", submitButton.getText());
 
-        Thread.sleep(3000);
-
         submitButton.click();
 
-        Thread.sleep(1000);
+        Assertions.assertDoesNotThrow(() -> {
+            this.driver.findElement(By.id("invalidMessage"));
+        });
+    }
+
+    private void signupWorkflow() {
+
+        this.driver.get("http://localhost:" + this.port + "/signup");
+
+        Assertions.assertEquals("Sign Up", driver.getTitle());
+
+        Assertions.assertDoesNotThrow(() -> {
+
+            WebElement fNameField = this.driver.findElement(By.id("inputFirstName"));
+
+            fNameField.sendKeys("Hello");
+
+            WebElement lNameField = this.driver.findElement(By.id("inputLastName"));
+
+            lNameField.sendKeys("World");
+
+            WebElement usrnameField = driver.findElement(By.id("inputUsername"));
+
+            usrnameField.sendKeys("byu00");
+
+            WebElement pwdField = driver.findElement(By.id("inputPassword"));
+
+            pwdField.sendKeys("test1234");
+
+            WebElement submitButton = driver.findElement(By.id("signupBtn"));
+
+            Assertions.assertEquals("Sign Up", submitButton.getText());
+
+            submitButton.click();
+        });
+    }
+
+    private void loginWorkflow() {
+
+        driver.get("http://localhost:" + this.port + "/login");
+
+        Assertions.assertEquals("Login", driver.getTitle());
 
         Assertions.assertThrows(NoSuchElementException.class, () -> {
             this.driver.findElement(By.id("invalidMessage"));
         });
+
+        WebElement usrnameField = driver.findElement(By.id("inputUsername"));
+
+        usrnameField.sendKeys("byu00");
+
+        WebElement pwdField = driver.findElement(By.id("inputPassword"));
+
+        pwdField.sendKeys("test1234");
+
+        WebElement submitButton = driver.findElement(By.id("loginBtn"));
+
+        Assertions.assertEquals("Login", submitButton.getText());
+
+        submitButton.click();
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            this.driver.findElement(By.id("invalidMessage"));
+        });
+    }
+
+    @Test
+    public void signupAndLoginWorkflow() {
+
+        this.signupWorkflow();
+
+        this.loginWorkflow();
+
+        this.driver.get("http://localhost:" + this.port + "/home");
+
+        Assertions.assertEquals("Home", driver.getTitle());
+
+        WebElement logoutButton = driver.findElement(By.id("logoutBtn"));
+
+        Assertions.assertEquals("Logout", logoutButton.getText());
+
+        logoutButton.click();
+
+        this.driver.get("http://localhost:" + this.port + "/home");
+
+        Assertions.assertEquals("Login", driver.getTitle());
     }
 }
