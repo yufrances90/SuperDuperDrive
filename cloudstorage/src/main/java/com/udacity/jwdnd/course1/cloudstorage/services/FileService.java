975a0622ecd36f01b289531f3851975305a2299a
Beijia(Frances) Yu <43866367+yufrances90@users.noreply.github.com>
2020-07-08 14:29:54 -0700

    feat: Implemented functions for checking if filename already exists for a specific user

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class FileService {
 
@@ -21,5 +24,15 @@ public class FileService {
 
         this.fileMapper = fileMapper;
         this.userFileMapper = userFileMapper;
+    }
+
+    public Boolean isFileNameAvailableForUser(String username, String filename) {
+
+        Map<String, Object> paraMap = new HashMap<>();
+
+        paraMap.put("username", username);
+        paraMap.put("filename", filename);
+
+        return this.userFileMapper.getFileByUsernameAndFileName(paraMap) == null;
     }
 }
